67a48466c64a42ff0446104d79d2d30bc9135b33
Feda Zidan <fido311@gmail.com>
2020-10-10 15:04:29 -0500

    Installed npm dotenv and saved my password in env file and tested the connection and it works.

--- employeeTraker.js
+++ employeeTraker.js
@@ -1,20 +1,19 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("dotenv").config();
 
 // mydql connection information
 var connection = mysql.createConnection({
     host: "localhost",
-
     port:3306,
     //Username and password
     user: "root",
-
-    password: "",
+    password: process.env.MYSQLPW,
     database: "employee_trackerDB"
 });
 
 // to test connection
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    console.log("connect as id " + connection.threadId);
 });
